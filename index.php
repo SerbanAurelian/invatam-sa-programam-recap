79409b24dc73e13e508fd7a369af5614704e794b
SerbanAurelian <a_serban83@yahoo.com>
2020-11-23 19:25:31 +0200

    added form

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
- <form>
+ <form method="POST" action="">
+ 
+	<input type="text" name="number" value="1">
+	
+	<input type="submit" name="save" value="Go">
 
- <input type="text" name="number" value="1">
-  <input type="submit" name="save" value="Go">
 </form> 
 
 <?php
