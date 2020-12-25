46a58b6616ed9579bc164d9b0a4a5edcb8cdac8b
SerbanAurelian <a_serban83@yahoo.com>
2020-12-25 20:03:20 +0200

    added login form. added tetails page and logput action

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
- <form method="POST" action="">
- 
-	<input type="text" name="number" value="1">
-	
-	<input type="submit" name="save" value="Go">
-
-</form> 
-
 <?php
 
-var_dump($_POST);
+session_start ();
+
+if (
+	isset($_POST['login']) &&
+	$_POST['username'] === 'admin' &&
+	$_POST['password'] === 'parola'
+) {
+	$_SESSION['logged'] = true;
+}
+
+$logged = false;
+if (isset($_SESSION['logged'])) {
+	$logged = true;
+}
+
+include 'menu.php';
+
+?>
+
+<?php if (!$logged): ?>
+
+<form method="POST"> 
+	<input type="text" name="username" value="" placeholder="Username">
+	<input type="password" name="password" value="" placeholder="Password">	
+	<input type="submit" name="login" value="Login">
+</form>
+
+<?php endif; ?>
