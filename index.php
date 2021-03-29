210723b5579f691024e9bc32e215b873319f4d3c
SerbanAurelian <a_serban83@yahoo.com>
2021-03-29 22:14:42 +0300

    hashed password, credentials error, hello user

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 session_start ();
 
+$error = '';
 if (
 	isset($_POST['login'])
 ) {
@@ -11,26 +12,25 @@ if (
 	$user = mysqli_fetch_assoc($result);
 	if ($user !== null) {
 		$_SESSION['logged'] = true;
+		$_SESSION['username'] = $_POST['username'];
+	} else {
+		$error = 'Invalid credentials!';
 	}
 }
 
-$logged = false;
-if (isset($_SESSION['logged'])) {
-	$logged = true;
-}
-
 include 'menu.php';
-
 ?>
-
 <?php if (!$logged): ?>
 
-<form method="POST"> 
-	<input type="text" name="username" value="" placeholder="Username">
-	<input type="password" name="password" value="" placeholder="Password">	
-	<input type="submit" name="login" value="Login">
-</form>
-
+	<form method="POST"> 
+		<input type="text" name="username" value="" placeholder="Username">
+		<input type="password" name="password" value="" placeholder="Password">	
+		<input type="submit" name="login" value="Login">
+	</form>
+	<?php if ($error !== ''): ?>
+		<p><?=$error?></p>
+	<?php endif; ?>
+	
 <?php endif; ?>
 
 <h1>Invatam Sa Programam</h1>
