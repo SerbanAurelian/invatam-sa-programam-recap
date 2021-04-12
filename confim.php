81337ba9ed4b1814faa269d10bdc082ae08c9ed8
SerbanAurelian <a_serban83@yahoo.com>
2021-04-12 23:12:20 +0300

    confirm.php, register.php

---
+++ confim.php
@@ -0,0 +1,19 @@
+<?php
+$message = '';
+$error = '';
+$code = $_GET['register_code'];
+
+$query = "SELECT id FROM users WHERE register_code = '".$code."'";
+
+$databaseConnection = mysqli_connect('localhost', 'root', '', 'invatam_sa_programam_recap');
+$result = mysqli_query($databaseConnection, $query);
+$user = mysqli_fetch_assoc($result);
+if ($user !== null) {
+	$query = "UPDATE users SER register_code = NULL WHERE id = ".(int)$user['id'];
+	$result = mysqli_query($databaseConnection, $query);
+	$message = 'Success';
+} else {
+	$error = 'Invalid register code!';
+}
+
+echo $message.'<br>'.$error;
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $error = '';
 if (
 	isset($_POST['login'])
 ) {
-	$query = "SELECT id FROM users WHERE username = '".$_POST['username']."' AND password = '".md5($_POST['password'])."'";
+	$query = "SELECT id FROM users WHERE username = '".$_POST['username']."' AND password = '".md5($_POST['password'])."'AND register_code IS NULL";
 	$databaseConnection = mysqli_connect('localhost', 'root', '', 'invatam_sa_programam_recap');
 	$result = mysqli_query($databaseConnection, $query);
 	$user = mysqli_fetch_assoc($result);
@@ -26,6 +26,7 @@ include 'menu.php';
 		<input type="text" name="username" value="" placeholder="Username">
 		<input type="password" name="password" value="" placeholder="Password">	
 		<input type="submit" name="login" value="Login">
+		<a href="register.php">Register</a>
 	</form>
 	<?php if ($error !== ''): ?>
 		<p><?=$error?></p>
