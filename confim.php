43c22880c99e4277c05604e61f20867d7efa39c1
SerbanAurelian <a_serban83@yahoo.com>
2021-04-26 18:09:19 +0300

    refactorizare

--- confim.php
+++ confim.php
@@ -16,4 +16,5 @@ if ($user !== null) {
 	$error = 'Invalid register code!';
 }
 
-echo $message.'<br>'.$error;
+header ('location: index.php?message='.$message.'&error='.$error);
+exit;
--- index.php
+++ index.php
@@ -1,47 +1,27 @@
 <?php
 
-session_start ();
+require 'includes/common.php';
 
 $error = '';
-if (
-	isset($_POST['login'])
-) {
+if (isset($_POST['login'])) {
 	$query = "SELECT id, register_code FROM users WHERE username = '".$_POST['username']."'AND password = '".md5($_POST['password'])."'AND register_code IS NULL";
+
 	$databaseConnection = mysqli_connect('localhost', 'root', '', 'invatam_sa_programam_recap');
 	$result = mysqli_query($databaseConnection, $query);
 	$user = mysqli_fetch_assoc($result);
 	if ($user !== null) {
-		$_SESSION['logged'] = true;
-		$_SESSION['username'] = $_POST['username'];
+		if ($user['register_code'] === null) {
+			$_SESSION['logged'] = true;
+			$_SESSION['username'] = $_POST['username'];
+			$logged = true;
+			$userName = $_SESSION['username'];
 		} else {
 			$error = 'Please confirm registration by clicking on the link you received at your email address!';
 		}
 	} else {
 		$error = 'Invalid credentials!';
 	}
+}
 
-include 'menu.php';
-?>
-<?php if (!$logged): ?>
-
-	<form method="POST"> 
-		<input type="text" name="username" value="" placeholder="Username">
-		<input type="password" name="password" value="" placeholder="Password">	
-		<input type="submit" name="login" value="Login">
-		<a href="register.php">Register</a>
-	</form>
-	<?php if ($error !== ''): ?>
-		<p><?=$error?></p>
-	<?php endif; ?>
-	
-<?php endif; ?>
-
-<?php if (isset($_GET['message'])): ?>
-	<p><?=$_GET['message']?></p>
-<?php endif; ?>
-
-<h1>Invatam Sa Programam</h1>
-
-<h3>PHP & MySQL</h3>
-
-<p>HTML</p>
+include 'views/menu.html.php';
+include 'views/home.html.php';
