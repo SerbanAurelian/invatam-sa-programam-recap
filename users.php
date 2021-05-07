a3ebe10a0657f1cc06a6bc64fb410887e2c8e34f
SerbanAurelian <a_serban83@yahoo.com>
2021-05-07 13:09:28 +0300

    login separat, stergerea utilizatorului

--- users.php
+++ users.php
@@ -6,9 +6,8 @@ if (!$logged) {
 	die('You are not logged');
 }
 
-$query = "SELECT id, username, register_code FROM users";
+$query = "SELECT id, username, email, register_code FROM users";
 
-$databaseConnection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 $result = mysqli_query($databaseConnection, $query);
 
 $users = [];
