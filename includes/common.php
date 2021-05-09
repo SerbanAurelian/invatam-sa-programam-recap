e721a7a46acb3759f3afd36abfe92d5b6e4670dc
SerbanAurelian <a_serban83@yahoo.com>
2021-05-10 00:25:36 +0300

    created gitignore and config.php, added config example

--- includes/common.php
+++ includes/common.php
@@ -2,11 +2,7 @@
 
 session_start();
 
-const SITE_URL = 'http://localhost/invatam-sa-programam-recap';
-const DB_HOST = 'localhost';
-const DB_USER = 'root';
-const DB_PASSWORD = '';
-const DB_NAME = 'invatam_sa_programam_recap';
+require 'config.php';
 
 function redirect ($uri)
 {
---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+
+const SITE_URL = 'http://localhost/invatam-sa-programam-recap';
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASSWORD = '';
+const DB_NAME = 'invatam_sa_programam_recap';
